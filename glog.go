2339435deb51ee94ea073ed8f123335bb78dc343
james.jeffrey <james.jeffrey@acronis.com>
2023-06-12 16:33:47 -0700

    add print printf and println call types

--- glog.go
+++ glog.go
@@ -63,32 +63,120 @@ func (l *Logger) SetPrefix(prefix string) {
 	l.prefix = prefix
 }
 
-func (l *Logger) Debug(msg string, args ...interface{}) {
-	l.write(LevelDebug, msg, args...)
+func (l *Logger) Print(args ...interface{}) {
+	l.Info(args...)
 }
 
-func (l *Logger) Info(msg string, args ...interface{}) {
-	l.write(LevelInfo, msg, args...)
+func (l *Logger) Println(args ...interface{}) {
+	l.Infoln(args...)
 }
 
-func (l *Logger) Warn(msg string, args ...interface{}) {
-	l.write(LevelWarn, msg, args...)
+func (l *Logger) Printf(msg string, args ...interface{}) {
+	l.Infof(msg, args...)
 }
 
-func (l *Logger) Error(msg string, args ...interface{}) {
-	l.write(LevelError, msg, args...)
+func (l *Logger) Debug(args ...interface{}) {
+	l.write(LevelDebug, args...)
 }
 
-func (l *Logger) Fatal(msg string, args ...interface{}) {
-	l.write(LevelFatal, msg, args...)
-	panic(fmt.Errorf(msg, args...))
+func (l *Logger) Info(args ...interface{}) {
+	l.write(LevelInfo, args...)
 }
 
-func (l *Logger) AtLevel(level int, msg string, args ...interface{}) {
-	l.write(level, msg, args...)
+func (l *Logger) Warn(args ...interface{}) {
+	l.write(LevelWarn, args...)
 }
 
-func (l *Logger) write(messageLevel int, msg string, args ...interface{}) error {
+func (l *Logger) Error(args ...interface{}) {
+	l.write(LevelError, args...)
+}
+
+func (l *Logger) Fatal(args ...interface{}) {
+	l.write(LevelFatal, args...)
+	panic(fmt.Errorf(fmt.Sprint(args...)))
+}
+
+func (l *Logger) Debugln(args ...interface{}) {
+	l.writeln(LevelDebug, args...)
+}
+
+func (l *Logger) Infoln(args ...interface{}) {
+	l.writeln(LevelInfo, args...)
+}
+
+func (l *Logger) Warnln(args ...interface{}) {
+	l.writeln(LevelWarn, args...)
+}
+
+func (l *Logger) Errorln(args ...interface{}) {
+	l.writeln(LevelError, args...)
+}
+
+func (l *Logger) Fatalln(args ...interface{}) {
+	l.writeln(LevelFatal, args...)
+	panic(fmt.Errorf(fmt.Sprint(args...)))
+}
+
+func (l *Logger) Debugf(msg string, args ...interface{}) {
+	l.writef(LevelDebug, msg, args...)
+}
+
+func (l *Logger) Infof(msg string, args ...interface{}) {
+	l.writef(LevelInfo, msg, args...)
+}
+
+func (l *Logger) Warnf(msg string, args ...interface{}) {
+	l.writef(LevelWarn, msg, args...)
+}
+
+func (l *Logger) Errorf(msg string, args ...interface{}) {
+	l.writef(LevelError, msg, args...)
+}
+
+func (l *Logger) Fatalf(msg string, args ...interface{}) {
+	l.writef(LevelFatal, msg, args...)
+	panic(fmt.Errorf(fmt.Sprint(args...)))
+}
+
+func (l *Logger) AtLevel(level int, args ...interface{}) {
+	l.write(level, args...)
+}
+
+func (l *Logger) AtLevelln(level int, args ...interface{}) {
+	l.writeln(level, args...)
+}
+
+func (l *Logger) AtLevelf(level int, msg string, args ...interface{}) {
+	l.writef(level, msg, args...)
+}
+
+func (l *Logger) writeln(messageLevel int, args ...interface{}) error {
+	if messageLevel&l.level == 0 {
+		return nil
+	}
+	levelPrefixes := []string{}
+	for level, prefix := range levels {
+		if l.level&level&messageLevel != 0 {
+			levelPrefixes = append(levelPrefixes, prefix)
+		}
+	}
+
+	if len(levelPrefixes) == 0 {
+		return nil
+	}
+
+	sort.Strings(levelPrefixes)
+
+	levelPrefix := strings.Join(levelPrefixes, "|")
+
+	if l.prefix != "" {
+		levelPrefix = fmt.Sprintf("%s|%s", levelPrefix, l.prefix)
+	}
+	_, err := fmt.Fprintf(l.w, "[%s] %s\n", levelPrefix, fmt.Sprintln(args...))
+	return err
+}
+
+func (l *Logger) writef(messageLevel int, msg string, args ...interface{}) error {
 	if messageLevel&l.level == 0 {
 		return nil
 	}
@@ -111,5 +199,31 @@ func (l *Logger) write(messageLevel int, msg string, args ...interface{}) error 
 		levelPrefix = fmt.Sprintf("%s|%s", levelPrefix, l.prefix)
 	}
 	_, err := fmt.Fprintf(l.w, "[%s] %s\n", levelPrefix, fmt.Sprintf(msg, args...))
+	return err
+}
+
+func (l *Logger) write(messageLevel int, args ...interface{}) error {
+	if messageLevel&l.level == 0 {
+		return nil
+	}
+	levelPrefixes := []string{}
+	for level, prefix := range levels {
+		if l.level&level&messageLevel != 0 {
+			levelPrefixes = append(levelPrefixes, prefix)
+		}
+	}
+
+	if len(levelPrefixes) == 0 {
+		return nil
+	}
+
+	sort.Strings(levelPrefixes)
+
+	levelPrefix := strings.Join(levelPrefixes, "|")
+
+	if l.prefix != "" {
+		levelPrefix = fmt.Sprintf("%s|%s", levelPrefix, l.prefix)
+	}
+	_, err := fmt.Fprintf(l.w, "[%s] %s", levelPrefix, fmt.Sprint(args...))
 	return err
 }
--- glog_test.go
+++ glog_test.go
@@ -10,69 +10,69 @@ import (
 func TestLevel(t *testing.T) {
 	buff := bytes.NewBuffer(nil)
 	l := NewLogger(buff, LevelDebug|LevelInfo)
-	require.NoError(t, l.write(LevelInfo, "Hello %q", "George"))
+	require.NoError(t, l.writef(LevelInfo, "Hello %q", "George"))
 	require.Equal(t, "[INFO] Hello \"George\"\n", buff.String())
 	buff.Reset()
-	require.NoError(t, l.write(LevelInfo|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(LevelInfo|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[DEBUG|INFO] Hello \"George\"\n", buff.String())
 
 	buff.Reset()
-	require.NoError(t, l.write(LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[DEBUG] Hello \"George\"\n", buff.String())
 
 	l.SetPrefix("K8s")
 
 	buff.Reset()
-	require.NoError(t, l.write(LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[DEBUG|K8s] Hello \"George\"\n", buff.String())
 
 	buff.Reset()
-	require.NoError(t, l.write(LevelError, "Hello %q", "George"))
+	require.NoError(t, l.writef(LevelError, "Hello %q", "George"))
 	require.Equal(t, "", buff.String())
 
 	const COMETS = 0x10000000
 	RegisterLevel(COMETS, "COMETS")
 
 	buff.Reset()
-	require.NoError(t, l.write(COMETS, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS, "Hello %q", "George"))
 	require.Equal(t, "", buff.String())
 
 	l.SetLevel(COMETS)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS, "Hello %q", "George"))
 	require.Equal(t, "[COMETS|K8s] Hello \"George\"\n", buff.String())
 
 	l.SetLevel(COMETS | LevelDebug)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS, "Hello %q", "George"))
 	require.Equal(t, "[COMETS|K8s] Hello \"George\"\n", buff.String())
 
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[COMETS|DEBUG|K8s] Hello \"George\"\n", buff.String())
 
 	l.SetLevel(COMETS)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[COMETS|K8s] Hello \"George\"\n", buff.String())
 
 	UnRegisterLevel(COMETS)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "", buff.String())
 
 	l.SetLevel(COMETS | LevelDebug)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[DEBUG|K8s] Hello \"George\"\n", buff.String())
 
 	UnRegisterLevel(LevelDebug)
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "", buff.String())
 
 	RegisterLevel(COMETS, "COMETS")
 	buff.Reset()
-	require.NoError(t, l.write(COMETS|LevelDebug, "Hello %q", "George"))
+	require.NoError(t, l.writef(COMETS|LevelDebug, "Hello %q", "George"))
 	require.Equal(t, "[COMETS|K8s] Hello \"George\"\n", buff.String())
 }
